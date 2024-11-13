4e3630567a749d7f5aaa440a32a92cb8b0888800
Maglunob15 <reynaldmaglunob@gmail.com>
2024-11-13 13:12:35 +0800

    Personal Portfolio

--- ActivitesProjects/WEB DEV/Assingment6/Assignment7.1- Maglunob, Reynald/js/assign1.js
+++
@@ -1,8 +0,0 @@
-let numberOfChildren = 3;
-let partnersName = "Reynald B. Maglunob"; 
-let geographicLocation = "Philippines"; 
-let jobTitle = "Web Developer"; 
-
-let fortuneMessage = `You will be a ${jobTitle} in ${geographicLocation} and married to ${partnersName} with ${numberOfChildren} kids.`;
-
-console.log(fortuneMessage);
